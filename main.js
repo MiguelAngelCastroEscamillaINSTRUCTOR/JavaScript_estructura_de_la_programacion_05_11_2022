6f275dc37dbf40a6d89aef5925b58153fb550ad1
MiguelAngelCastroEscamillaINSTRUCTOR <macastro40@misena.edu.co>
2022-11-05 17:28:35 -0500

    Operador condicional (ternario)

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 addEventListener("DOMContentLoaded", (e)=>{
 
-   $edad = 17; 
+   $edad = 18; 
 
     if($edad==18){ //si
         console.log("Usted cumplio la mayoria de edad");
+        console.log(":)");
     }else if($edad>=20 && $edad<=30){ // sino
         console.log("Usted es una persona adulta joven");
     }else{ //no
@@ -14,6 +15,16 @@ addEventListener("DOMContentLoaded", (e)=>{
         }
     }
     
+    let fn = ()=>{ 
+        console.warn("Usted cumplio la mayoria de edad");
+        return ":)";
+    }
+    $res = ($edad==18) ? fn()
+           :($edad>=20 && $edad<=30)? "Usted es una persona adulta joven"
+           :($edad==19) ? "Usted es mayor de edad"
+           : "....";
+    
+    console.warn($res)
 
 
 })
